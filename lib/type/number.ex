69027362b35a44166d54ce693b23fe3492be9d06
Shraddha <shraddha@lob.com>
2018-07-27 15:57:05 -0700

    added test cases for Number

--- lib/type/number.ex
+++ lib/type/number.ex
@@ -7,54 +7,116 @@ defmodule Litmus.Type.Number do
     :min,
     :max,
     integer: false,
-    required: false
+    required: false,
+    convert: true
   ]
 
   @type t :: %__MODULE__{
-          min: non_neg_integer,
-          max: non_neg_integer,
+          min: non_neg_integer | nil,
+          max: non_neg_integer | nil,
           integer: boolean,
-          required: boolean
+          required: boolean,
+          convert: boolean
         }
+
+  alias Litmus.Required
 
   @spec validate_field(t, binary, map) :: {:ok, map} | {:error, binary}
   def validate_field(type, field, data) do
     with {:ok, data} <- Required.validate(type, field, data),
-          {:ok, data} <- min_validate(type, field, data),
-          {:ok, data} <- max_validate(type, field, data),
-          {:ok, data} <- integer_validate(type, field, data) do
+         {:ok, data} <- convert(type, field, data),
+         {:ok, data} <- min_validate(type, field, data),
+         {:ok, data} <- max_validate(type, field, data),
+         {:ok, data} <- integer_validate(type, field, data) do
       {:ok, data}
     else
       {:error, msg} -> {:error, msg}
     end
   end
 
-  @spec min_validate(t, binary, map) :: {:ok, map} | {:error, binary}
-  defp min_validate(%__MODULE__{min: min}, field, params)
-       when min != nil and is_integer(min) and min >= 0 do
-    if Map.has_key?(params, field) && String.length(params[field]) < min do
-      {:error, "#{field} length must be greater than or equal to #{min} characters"}
+  @spec string_to_number(binary) :: number | :error
+  defp string_to_number(str) do
+    modified_str =
+      try do
+        String.to_integer(str)
+      rescue
+        ArgumentError ->
+          try do
+            String.to_float("0" <> str)
+          rescue
+            ArgumentError ->
+              :error
+          end
+      end
+
+    modified_str
+  end
+
+  @spec convert(t, binary, map) :: {:ok, map} | {:error, binary}
+  defp convert(%__MODULE__{convert: false}, _field, params) do
+    {:ok, params}
+  end
+
+  defp convert(%__MODULE__{convert: true}, field, params) do
+    if Map.has_key?(params, field) && !(is_binary(params[field]) || is_number(params[field])) do
+      {:error, "#{field} must be a valid number"}
+    else
+      if is_binary(params[field]) do
+        modified_value = string_to_number(params[field])
+
+        case modified_value do
+          :error ->
+            {:error, "#{field} must be a valid number"}
+
+          _ ->
+            modified_params = Map.put(params, field, modified_value)
+            {:ok, modified_params}
+        end
+      else
+        {:ok, params}
+      end
+    end
+  end
+
+  @spec integer_validate(t, binary, map) :: {:ok, map} | {:error, binary}
+  defp integer_validate(%__MODULE__{integer: false}, _field, params) do
+    {:ok, params}
+  end
+
+  defp integer_validate(%__MODULE__{integer: true}, field, params) do
+    if Map.has_key?(params, field) && !is_integer(params[field]) do
+      {:error, "#{field} must be an integer"}
     else
       {:ok, params}
     end
   end
 
+  @spec min_validate(t, binary, map) :: {:ok, map} | {:error, binary}
   defp min_validate(%__MODULE__{min: nil}, _field, params) do
     {:ok, params}
   end
 
-  @spec max_validate(t, binary, map) :: {:ok, map} | {:error, binary}
-  defp max_validate(%__MODULE__{max: max}, field, params)
-       when max != nil and is_integer(max) and max >= 0 do
-    if Map.has_key?(params, field) && String.length(params[field]) > max do
-      {:error, "#{field} length must be less than or equal to #{max} characters"}
+  defp min_validate(%__MODULE__{min: min}, field, params)
+       when is_number(min) and min >= 0 do
+    if Map.has_key?(params, field) && params[field] < min do
+      {:error, "#{field} must be greater than or equal to #{min}"}
     else
       {:ok, params}
     end
   end
 
+  @spec max_validate(t, binary, map) :: {:ok, map} | {:error, binary}
   defp max_validate(%__MODULE__{max: nil}, _field, params) do
     {:ok, params}
+  end
+
+  defp max_validate(%__MODULE__{max: max}, field, params)
+       when is_number(max) and max >= 0 do
+    if Map.has_key?(params, field) && params[field] > max do
+      {:error, "#{field} must be less than or equal to #{max}"}
+    else
+      {:ok, params}
+    end
   end
 
   defimpl Litmus.Type do
---
+++ test/type/number_test.exs
@@ -0,0 +1,138 @@
+defmodule Litmus.Type.NumberTest do
+  use ExUnit.Case
+
+  alias Litmus.Type
+
+  describe "validate_field/3" do
+    test "validates property values of data based on their Number schema definition in Type.Number module" do
+      field = "id"
+      data = %{"id" => 1}
+
+      type = %Type.Number{
+        required: true
+      }
+
+      assert Type.Number.validate_field(type, field, data) == {:ok, data}
+    end
+  end
+
+  describe "convert string to number" do
+    test "returns :ok with modified value when convert is true" do
+      data = %{"id" => ".6"}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          convert: true
+        }
+      }
+
+      modified_data = %{"id" => 0.6}
+      assert Litmus.validate(data, schema) == {:ok, modified_data}
+    end
+
+    test "errors when convert is true and field type is neither number or string" do
+      field = "id"
+      data = %{"id" => "1.a"}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          convert: true
+        }
+      }
+
+      assert Litmus.validate(data, schema) == {:error, "#{field} must be a valid number"}
+    end
+  end
+
+  describe "minimum validation" do
+    test "returns :ok when field is more than or equal to min value" do
+      data = %{"id" => 6}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          required: true,
+          min: 3
+        }
+      }
+
+      assert Litmus.validate(data, schema) == {:ok, data}
+    end
+
+    test "errors when field is less than min" do
+      min = 3
+      field = "id"
+      data = %{"id" => 1}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          required: true,
+          min: min
+        }
+      }
+
+      assert Litmus.validate(data, schema) ==
+               {:error, "#{field} must be greater than or equal to #{min}"}
+    end
+  end
+
+  describe "maximum validation" do
+    test "returns :ok when field is less than or equal to max" do
+      max = 3
+      data = %{"id" => 1}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          required: true,
+          max: max
+        }
+      }
+
+      assert Litmus.validate(data, schema) == {:ok, data}
+    end
+
+    test "errors when field is more than max" do
+      max = 3
+      field = "id"
+      data = %{"id" => 6}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          required: true,
+          max: max
+        }
+      }
+
+      assert Litmus.validate(data, schema) ==
+               {:error, "#{field} must be less than or equal to #{max}"}
+    end
+  end
+
+  describe "integer type validation" do
+    test "returns :ok when field is an integer and the schema property integer is set to true" do
+      data = %{"id" => 1}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          required: true,
+          integer: true
+        }
+      }
+
+      assert Litmus.validate(data, schema) == {:ok, data}
+    end
+
+    test "errors when field is not an integer and the schema property integer is set to true" do
+      field = "id"
+      data = %{"id" => 1.6}
+
+      schema = %{
+        "id" => %Litmus.Type.Number{
+          required: true,
+          integer: true
+        }
+      }
+
+      assert Litmus.validate(data, schema) == {:error, "#{field} must be an integer"}
+    end
+  end
+end
