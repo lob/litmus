9c4b4d976e4e71aeaf96f65fa642521b3f0785f7
Shraddha <shraddha@lob.com>
2018-07-27 11:17:56 -0700

    feat(boolean-validation): Added validation functions for Boolean data type

--- lib/type/boolean.ex
+++ lib/type/boolean.ex
@@ -1,10 +1,13 @@
 defmodule Litmus.Type.Boolean do
-  @moduledoc """
-  Schema and validation for Boolean data type.
-  """
+  @moduledoc false
 
-  defstruct truthy: [true, "true"],
-            falsy: [false, "false"],
+  alias Litmus.Required
+
+  @truthy_default [true, "true"]
+  @falsy_default [false, "false"]
+
+  defstruct truthy: @truthy_default,
+            falsy: @falsy_default,
             required: false
 
   @type t :: %__MODULE__{
@@ -12,4 +15,42 @@ defmodule Litmus.Type.Boolean do
           falsy: [term],
           required: boolean
         }
+
+  @spec validate_field(t, binary, map) :: {:ok, map} | {:error, binary}
+  def validate_field(type, field, data) do
+    with {:ok, data} <- Required.validate(type, field, data),
+         {:ok, data} <- truthy_validate(type, field, data),
+         {:ok, data} <- falsy_validate(type, field, data) do
+      {:ok, data}
+    else
+      {:error, msg} -> {:error, msg}
+    end
+  end
+
+  @spec truthy_validate(t, binary, map) :: {:ok, map} | {:error, binary}
+  defp truthy_validate(%__MODULE__{truthy: truthy}, field, params) do
+    if Map.has_key?(params, field) && !(params[field] in Enum.uniq(truthy ++ @truthy_default)) do
+      {:error, "#{field} must be boolean value"}
+    else
+      modified_params = Map.replace!(params, field, true)
+      {:ok, modified_params}
+    end
+  end
+
+  @spec falsy_validate(t, binary, map) :: {:ok, map} | {:error, binary}
+  defp falsy_validate(%__MODULE__{falsy: falsy}, field, params) do
+    if Map.has_key?(params, field) && !(params[field] in Enum.uniq(falsy ++ @falsy_default)) do
+      {:error, "#{field} must be boolean value"}
+    else
+      modified_params = Map.replace!(params, field, false)
+      {:ok, modified_params}
+    end
+  end
+
+  defimpl Litmus.Type do
+    alias Litmus.Type
+
+    @spec validate(Type.t(), binary, map) :: {:ok, map} | {:error, binary}
+    def validate(type, field, data), do: Type.Boolean.validate_field(type, field, data)
+  end
 end
