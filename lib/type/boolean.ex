c35464b8d330f0b629cb879b18ec90ee1afa8829
Shraddha <shraddha@lob.com>
2018-07-30 17:22:42 -0700

    allow case insensitive check

--- lib/type/boolean.ex
+++ lib/type/boolean.ex
@@ -26,16 +26,43 @@ defmodule Litmus.Type.Boolean do
     end
   end
 
+  @spec check_boolean_values(term, [term], [term]) :: {:ok, map} | nil
+  def check_boolean_values(initial_value, additional_values, default_values)
+      when is_binary(initial_value) do
+    allowed_values = Enum.uniq(additional_values ++ default_values)
+
+    allowed_values =
+      Enum.map(allowed_values, fn item ->
+        if is_binary(item) do
+          String.downcase(item)
+        end
+      end)
+
+    if String.downcase(initial_value) in allowed_values do
+      true
+    else
+      nil
+    end
+  end
+
+  def check_boolean_values(initial_value, additional_values, default_values) do
+    if initial_value in Enum.uniq(additional_values ++ default_values) do
+      true
+    else
+      nil
+    end
+  end
+
   @spec truthy_falsy_validate(t, binary, map) :: {:ok, map} | {:error, binary}
   defp truthy_falsy_validate(%__MODULE__{falsy: falsy, truthy: truthy}, field, params) do
     cond do
       !Map.has_key?(params, field) ->
         {:ok, params}
 
-      params[field] in Enum.uniq(truthy ++ @truthy_default) ->
+      check_boolean_values(params[field], truthy, @truthy_default) ->
         {:ok, Map.replace!(params, field, true)}
 
-      params[field] in Enum.uniq(falsy ++ @falsy_default) ->
+      check_boolean_values(params[field], falsy, @falsy_default) ->
         {:ok, Map.replace!(params, field, false)}
 
       true ->
--- test/type/boolean_test.exs
+++ test/type/boolean_test.exs
@@ -19,16 +19,18 @@ defmodule Litmus.Type.BooleanTest do
   describe "truthy validation" do
     test "returns :ok with value converted to true when field is in additional truthy values that are considered to be valid booleans" do
       data = %{"id_given" => 1}
+      case_data = %{"id_given" => "onE"}
       modified_data = %{"id_given" => true}
 
       schema = %{
         "id_given" => %Litmus.Type.Boolean{
-          truthy: [1]
+          truthy: [1, "One"]
         },
         "new_user" => %Litmus.Type.Boolean{}
       }
 
       assert Litmus.validate(data, schema) == {:ok, modified_data}
+      assert Litmus.validate(case_data, schema) == {:ok, modified_data}
     end
 
     test "errors when field is not in additional truthy values that are considered to be valid booleans" do
@@ -46,7 +48,7 @@ defmodule Litmus.Type.BooleanTest do
   end
 
   describe "falsy validation" do
-    test "returns :ok with value converted to true when field is in additional falsy values that are considered to be valid booleans" do
+    test "returns :ok with value converted to false when field is in additional falsy values that are considered to be valid booleans" do
       data = %{"id_given" => 0}
       modified_data = %{"id_given" => false}
 
